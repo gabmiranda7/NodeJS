3d08cd78d84bf522a3c7a0d80f99ee7f348b3932
Gabriel Miranda <gab.mirandaatk75@gmail.com>
2024-11-03 20:55:44 -0300

    Aula 1

---
+++ 1 - Node na prática/mat.js
@@ -0,0 +1,8 @@
+var num = 700
+var num2 = 110
+
+function somar(a, b){
+    return a + b;
+}
+
+console.log(somar(num, num2))
---
+++ 1 - Node na prática/nome.js
@@ -0,0 +1,4 @@
+var Nome = "Gabriel"
+var Sobrenome = "Miranda"
+
+console.log(Nome + " " + Sobrenome);
