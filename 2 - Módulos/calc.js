0b7bf9ee2001456053b9ac7241d4580701e64602
Gabriel Miranda <gab.mirandaatk75@gmail.com>
2024-11-03 20:56:53 -0300

    Aula 2

---
+++ 2 - Módulos/calc.js
@@ -0,0 +1,9 @@
+var SomaFunc = require("./somar");
+var SubFunc = require("./subtrair");
+var MultiFunc = require("./multiplicar");
+var DivFunc = require("./dividir");
+
+console.log (SomaFunc(10,2))
+console.log (SubFunc(10,2))
+console.log (MultiFunc(10,2))
+console.log (DivFunc(10,2))
